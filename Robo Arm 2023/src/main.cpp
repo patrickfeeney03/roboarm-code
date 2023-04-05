3df90735401fedb03bb42722443752e8e14101f9
Patrick <g00409592@atu.ie>
2023-04-05 15:31:09 +0100

    Control arm via Serial Terminal

--- Robo Arm 2023/src/main.cpp
+++ Robo Arm 2023/src/main.cpp
@@ -11,15 +11,67 @@ Servo wrist;
 Servo rotateWrist;
 Servo claws;
 
+void moveServos();
+void clearSerialBuffer();
+
 void setup() {
+  Serial.begin(115200);
+
   base.attach(BASE_PIN);
   shoulder.attach(SHOULDER_PIN);
   elbow.attach(ELBOW_PIN);
   wrist.attach(WRIST_PIN);
   rotateWrist.attach(ROTATE_WRIST_PIN);
-  claws.attach(CLAWS_PIN);  
+  claws.attach(CLAWS_PIN);
 }
 
 void loop() {
+  moveServos();
+  Serial.println("Where even am I?");
+}
+
+void moveServos() {
+  clearSerialBuffer();
+  Serial.print("1 - 6 to select servo: ");
+  while (Serial.available() == 0);
+  uint8_t servoNum = Serial.parseInt();
+  Serial.print("servoNum: ");
+  Serial.println(servoNum);
   
+  clearSerialBuffer();
+  Serial.print("\nEnter angle: ");
+  while (Serial.available() == 0);
+  uint8_t angle = Serial.parseInt();
+  Serial.print("Angle: ");
+  Serial.println(angle);
+
+
+  switch (servoNum) {
+    case 1:
+      base.write(angle);
+      break;
+    case 2:
+      shoulder.write(angle);
+      break;
+    case 3:
+      elbow.write(angle);
+      break;
+    case 4:
+      wrist.write(angle);
+      break;
+    case 5:
+      rotateWrist.write(angle);
+      break;
+    case 6:
+      claws.write(angle);
+      break;
+    default:
+      printf("Something broke\n");
+  }
+}
+
+void clearSerialBuffer() {
+  while (Serial.available()  > 0 ) {
+    Serial.read();
+  }
 }
