084d2ac442319d97b97507165e14d85fb7c4e20e
Patrick <g00409592@atu.ie>
2023-04-05 14:49:20 +0100

    Create Servo objects

--- Robo Arm 2023/src/main.cpp
+++ Robo Arm 2023/src/main.cpp
@@ -1,7 +1,15 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
 
 const uint8_t BASE_PIN = 15, SHOULDER_PIN = 16, ELBOW_PIN = 17;
 const uint8_t WRIST_PIN = 18, ROTATE_WRIST_PIN = 19, CLAWS_PIN = 21;
+
+Servo base;
+Servo shoulder;
+Servo elbow;
+Servo wrist;
+Servo rotateWrist;
+Servo claws;
 
 void setup() {
   
