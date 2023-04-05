84aa42ffaf95db019db051d4b88ef784faa12a55
Patrick <g00409592@atu.ie>
2023-04-05 14:41:08 +0100

    Create constants for servo pins

--- Robo Arm 2023/src/main.cpp
+++ Robo Arm 2023/src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 
+const uint8_t BASE_PIN = 15, SHOULDER_PIN = 16, ELBOW_PIN = 17;
+const uint8_t WRIST_PIN = 18, ROTATE_WRIST_PIN = 19, CLAWS_PIN = 21;
+
 void setup() {
-  // put your setup code here, to run once:
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
